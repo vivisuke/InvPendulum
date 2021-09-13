fbf493ff25df2e6e58e6210055c78dc206c82312
vivisuke <ntsuda@master.email.ne.jp>
2021-09-13 10:26:16 +0900

    0004 ◎ C 09/13 09/13 0.001│ToDo│倒立振子角度・角速度表示

--- InvPendulum/MainScene.gd
+++ InvPendulum/MainScene.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 var wheelX = 320.0
 var wheelVelocity = 0.0
-var barAngle = 90.0
+var barAngle = 90.0				# 単位：degree
 var angularVelocity = 0.0
 
+var rng = RandomNumberGenerator.new()
+
 func _ready():
+	rng.randomize()
 	pass # Replace with function body.
 
 func _physics_process(delta):
@@ -13,6 +16,16 @@ func _physics_process(delta):
 	$WheelXLabel.text = "WX: %.1f" % $Wheel.position.x
 	$WheelVLabel.text = "WX: %.1f" % wheelVelocity
 	wheelX = $Wheel.position.x
-	if wheelVelocity == 0.0 && barAngle == 90.0 && angularVelocity == 0.0:
-		$Wheel.add_central_force(Vector2(10, 0)*delta)
+	var ba = ($Bar.rotation * 180 / PI)
+	$BarALavel.text = "BarA: %.1f" % ba
+	$BarAVLavel.text = "BarAV: %.1f" % ((ba - barAngle) / delta)
+	barAngle = ba	
+	#
+	if wheelVelocity == 0.0 && round(barAngle) == 90 && angularVelocity == 0.0:
+		if rng.randi_range(0, 1) == 0:
+			print("add_central_force(+400)")
+			$Wheel.add_central_force(Vector2(400, 0)*delta)
+		else:
+			print("add_central_force(-400)")
+			$Wheel.add_central_force(Vector2(-400, 0)*delta)
 	pass
