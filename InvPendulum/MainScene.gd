5491cbd1d4215b8a85bae2345d834849fb82c669
vivisuke <ntsuda@master.email.ne.jp>
2021-09-13 09:57:21 +0900

    0003 ◎ C 09/13 09/13 0.001│ToDo│ホイールX位置・速度表示

--- InvPendulum/MainScene.gd
+++ InvPendulum/MainScene.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var wheelX = 320.0
 var wheelVelocity = 0.0
 var barAngle = 90.0
 var angularVelocity = 0.0
@@ -8,6 +9,10 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
+	wheelVelocity = ($Wheel.position.x - wheelX) / delta
+	$WheelXLabel.text = "WX: %.1f" % $Wheel.position.x
+	$WheelVLabel.text = "WX: %.1f" % wheelVelocity
+	wheelX = $Wheel.position.x
 	if wheelVelocity == 0.0 && barAngle == 90.0 && angularVelocity == 0.0:
 		$Wheel.add_central_force(Vector2(10, 0)*delta)
 	pass
