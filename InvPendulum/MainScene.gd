f296ef655c5cbfe0219e739c760c03f7fec104d3
vivisuke <ntsuda@master.email.ne.jp>
2021-09-13 13:02:13 +0900

    0006 ◎ C 09/13 09/13 0.001│ToDo│log2() 関数定義

--- InvPendulum/MainScene.gd
+++ InvPendulum/MainScene.gd
@@ -10,16 +10,34 @@ var rng = RandomNumberGenerator.new()
 func _ready():
 	rng.randomize()
 	pass # Replace with function body.
-
+func log2(x) -> int:
+	#x = round(x)
+	if round(x) == 0: return 0
+	var positive = true
+	if x < 0:
+		positive = false
+		x = -x
+	var l2 = round(log(x) / log(2.0))
+	#if l2 > 7: l2 = 7
+	if positive:
+		return int(l2)
+	else:
+		return -int(l2)
 func _physics_process(delta):
 	wheelVelocity = ($Wheel.position.x - wheelX) / delta
 	$WheelXLabel.text = "WX: %.1f" % $Wheel.position.x
-	$WheelVLabel.text = "WX: %.1f" % wheelVelocity
+	$WheelVLabel.text = "WV: %.1f" % wheelVelocity
 	wheelX = $Wheel.position.x
 	var ba = ($Bar.rotation * 180 / PI)
 	$BarALavel.text = "BarA: %.1f" % ba
-	$BarAVLavel.text = "BarAV: %.1f" % ((ba - barAngle) / delta)
-	barAngle = ba	
+	var bav = (ba - barAngle) / delta
+	$BarAVLavel.text = "BarAV: %.1f" % bav
+	barAngle = ba
+	#
+	print("WX: ", clamp(log2(($Wheel.position.x-320)/10), -7, 7))
+	print("WV: ", clamp(log2(wheelVelocity), -7, 7))
+	print("BarA: ", clamp(log2(barAngle - 90), -7, 7))
+	print("BarAV: ", clamp(log2(bav), -7, 7))
 	#
 	if wheelVelocity == 0.0 && round(barAngle) == 90 && angularVelocity == 0.0:
 		if rng.randi_range(0, 1) == 0:
